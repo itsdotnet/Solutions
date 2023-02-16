e55f3f908c9518e503587483dfee1f85c4e90bcb
iskandar_kodirov <eiscocyber@gmail.com>
2023-02-16 09:27:27 +0500

    Sonlar tartib raqami summasi

---
+++ harf_sum.py
@@ -0,0 +1,15 @@
+dic_abc = {'a':'1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8','i':'9','j':'10','k':'11','l':'12','m':'13','n':'14','o':'15','p':'16','q':'17','r':'18','s':'19','t':'20','u':'21','v':'22','w':'23','x':'24','y':'25','z':'26'}
+dic_ABS = {'A':'1','B':'2','C':'3','D':'4','E':'5','F':'6','G':'7','H':'8','I':'9','J':'10','K':'11','L':'12','M':'13','N':'14','O':'15','P':'16','Q':'17','R':'18','S':'19','T':'20','U':'21','V':'22','W':'23','X':'24','Y':'25','Z':'26'}
+ism = list(input())
+familiya = list(input())
+for i in range(len(ism)):
+    if ism[i].islower():
+        ism[i] = dic_abc[ism[i]]
+    else:
+        ism[i] = dic_ABS[ism[i]]
+for i in range(len(familiya)):
+    if familiya[i].islower():
+        familiya[i] = dic_abc[familiya[i]]
+    else:
+        familiya[i] = dic_ABS[familiya[i]]
+print(f"{''.join(ism)} + {''.join(familiya)} = {int(''.join(ism))+int(''.join(familiya))}")
